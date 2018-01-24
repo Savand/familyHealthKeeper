7134e86252e0c493b53b56151cabb4cefab7e1b1
Ilia Kochedikov <kochedikov.ilia@gmail.com>
2018-01-24 18:36:10 +0200

    add testGetPersonPage() method to PersonControllerTest

--- src/test/java/com/savand/controller/PersonControllerTest.java
+++ src/test/java/com/savand/controller/PersonControllerTest.java
@@ -2,42 +2,41 @@ package com.savand.controller;
 
 import com.savand.model.Person;
 import com.savand.service.PersonService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-
+@WebMvcTest(PersonController.class)
 public class PersonControllerTest {
 
-    @Mock
+    @MockBean
     private PersonService personService;
 
-    @InjectMocks
-    private PersonController personController;
+//    @InjectMocks
+//    private PersonController personController;
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
-    }
+//    @Before
+//    public void setup() {
+//        MockitoAnnotations.initMocks(this);
+//        mockMvc = MockMvcBuilders.standaloneSetup(personController).build();
+//    }
 
     @Test
     public void testGetPersonList() throws Exception {
@@ -47,15 +46,28 @@ public class PersonControllerTest {
 
         when(personService.getPersonListOrderByIdAsc()).thenReturn((List) persons);
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/persons"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("personList"))
-                .andExpect(model().attribute("personList", hasSize(2)));
+                get("/persons"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("personList"))
+                .andExpect(model().attribute("personList", persons))
+                .andDo(print());
 
     }
 
+    //Todo: finish the test (error message = Required Integer parameter 'id' is not present)
     @Test
-    public void testGetPersonPage() {
+    public void testGetPersonPage() throws Exception {
+        Person person = new Person();
+
+        when(personService.getPersonById(10007)).thenReturn(person);
+        Integer value = 10007;
+        mockMvc.perform(
+                get("/personPage").requestAttr("id", value))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("person", is(person)))
+                .andExpect(view().name("personPage")
+                )
+                ;
 
     }
 }
